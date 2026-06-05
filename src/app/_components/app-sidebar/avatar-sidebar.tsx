f6eec3418a76353f1d8ee9de62562259b99381b6
Yusril Adriansyah Putra <yusriladr.37@gmail.com>
2026-06-05 08:39:18 +0700

    feat: :sparkles: add apidog and avatar sidebar on full width

--- src/app/_components/app-sidebar/avatar-sidebar.tsx
+++ src/app/_components/app-sidebar/avatar-sidebar.tsx
@@ -26,7 +26,7 @@ export function AvatarSidebar() {
     <SidebarMenu>
       <SidebarMenuItem>
         <DropdownMenu>
-          <DropdownMenuTrigger>
+          <DropdownMenuTrigger className="w-full">
             <SidebarMenuButton
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
